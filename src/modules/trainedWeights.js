f31731a7f6ef398196fdb36a3a5febb6fc65cfc6
Chase Lester <chaselester@overstock.com>
2019-12-02 22:07:11 -0700

    added comment in trainedWeights.js

--- src/modules/trainedWeights.js
+++ src/modules/trainedWeights.js
@@ -3,6 +3,8 @@ import train from './train';
 import { generatePoints, randomPoints, randomWeights } from '../utils/generatePoints';
 
 const trainedWeights = async () => {
+    // gen generates brand new points to be used in examples where rand uses same points as in circlePoint.js
+    // not sure if one is to be used over the other. Fun Fun Function tut used gen I believe
     const gen = generatePoints();
     const rand = randomPoints;
     const examples = gen.map(point => ({
